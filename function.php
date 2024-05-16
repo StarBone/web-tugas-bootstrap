77d3e1a022585383364c9f2b9dc0eac3df593f81
StarBone <anax.tore44@gmail.com>
2024-05-16 18:37:54 +0700

    first commit

---
+++ function.php
@@ -0,0 +1,65 @@
+<?php
+
+session_start();
+
+// Koneksi
+$con = mysqli_connect('localhost','root','','kasir');
+
+// Login
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $check = mysqli_query($con, "SELECT * FROM user WHERE username='$username' AND password='$password' ");
+    $count = mysqli_num_rows($check);
+
+    if ($count > 0) {
+        $_SESSION['login'] = 'True';
+        header('location:index.php');
+    }
+    else {
+        echo '
+        <script>alert("Username dan Password salah");
+        window.location.href="login.php"
+        </script>';
+    }
+}
+
+if (isset($_POST['tambahbarang'])) {    
+    $namaproduk = $_POST['nama_produk'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga = $_POST['harga'];
+    $stok = $_POST['stok'];
+
+    $insert = mysqli_query($con, "INSERT INTO produk (nama_produk,deskripsi,stok,harga) VALUES ('$namaproduk','$deskripsi','$stok','$harga')");
+
+    if ($insert) {
+        header('location:stock.php');
+    }
+    else {
+        echo '
+        <script>alert("Gagal Menambah Barang");
+        window.location.href="stock.php"
+        </script>';
+    }
+}
+
+if (isset($_POST['tambahpelanggan'])) {    
+    $namapelanggan = $_POST['nama_pelanggan'];
+    $no_telp = $_POST['no_telp'];
+    $alamat = $_POST['alamat'];
+
+    $insert = mysqli_query($con, "INSERT INTO pelanggan (nama_pelanggan,no_telp,alamat) VALUES ('$namapelanggan','$no_telp','$alamat')");
+
+    if ($insert) {
+        header('location:pelanggan.php');
+    }
+    else {
+        echo '
+        <script>alert("Gagal Menambah Pelanggan");
+        window.location.href="stock.php"
+        </script>';
+    }
+}
+
+?>
---
+++ kasir.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Apr 2024 pada 11.54
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail pesanan`
+--
+
+CREATE TABLE `detail pesanan` (
+  `iddetailpesanan` int(11) NOT NULL,
+  `idpesanan` int(11) DEFAULT NULL,
+  `idproduk` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `masuk`
+--
+
+CREATE TABLE `masuk` (
+  `idmasuk` int(11) NOT NULL,
+  `idproduk` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `idpelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(50) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `idpesanan` int(11) NOT NULL,
+  `tanggal` timestamp NULL DEFAULT current_timestamp(),
+  `iduser` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int(11) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `deskripsi` varchar(50) NOT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `nama_produk`, `deskripsi`, `harga`, `stok`) VALUES
+(1, 'Roti Tawar', 'tidak ada rasa', 10000, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`iduser`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `detail pesanan`
+--
+ALTER TABLE `detail pesanan`
+  ADD PRIMARY KEY (`iddetailpesanan`);
+
+--
+-- Indeks untuk tabel `masuk`
+--
+ALTER TABLE `masuk`
+  ADD PRIMARY KEY (`idmasuk`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`idpelanggan`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`idpesanan`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `detail pesanan`
+--
+ALTER TABLE `detail pesanan`
+  MODIFY `iddetailpesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `masuk`
+--
+ALTER TABLE `masuk`
+  MODIFY `idmasuk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `idpelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `idpesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
