4cd346b5ba88e8f22351b2015654f0677a595fc4
StarBone <anax.tore44@gmail.com>
2024-06-09 16:52:07 +0700

    hsn

--- menu.php
+++ menu.php
@@ -2,20 +2,14 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>IBUKU BAKERY</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
-    <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"
-      rel="stylesheet"
-    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,9 +20,9 @@
   </head>
   <body class="bg-lightcream">
     <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-darkbrown bg-darkbrown">
+    <nav class="navbar navbar-expand-lg navbar-darkbrown bg-transparent fixed-top transparent">
       <div class="container px-4 px-lg-5">
-        <img src="./assets/logo.png" alt="IBUKU BAKERY" href="menu.php">
+        <a href="menu.php"><img src="./assets/logo.png" alt="IBUKU BAKERY"></a>
         <button
           class="navbar-toggler"
           type="button"
@@ -66,36 +60,47 @@
         </div>
       </div>
     </nav>
-    <!-- Header-->
-    <header>
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active c-item">
-            <img src="./assets/img/header/5.png" class="d-block w-100 c-img" alt="...">
+    
+    <!-- Carousel -->
+    <div id="hero-carousel" class="carousel slide shadow" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./assets/img/carousel/1.jpg" class="d-block w-100" alt="Slide 1">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Freshly Baked Breads</h5>
+            <p>Enjoy our variety of freshly baked breads.</p>
           </div>
-          <div class="carousel-item">
-            <img src="./assets/img/header/6.jpg" class="d-block w-100 c-img" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/img/carousel/2.jpg" class="d-block w-100" alt="Slide 2">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Delicious Cakes</h5>
+            <p>Perfect for every occasion.</p>
           </div>
-          <div class="carousel-item">
-            <img src="./assets/img/header/7.jpg" class="d-block w-100 c-img" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="./assets/img/carousel/3.jpg" class="d-block w-100" alt="Slide 3">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Exquisite Pastries</h5>
+            <p>Treat yourself with our delicate pastries.</p>
           </div>
         </div>
       </div>
-    </header>
+    </div>
+
     <!-- Section-->
     <section class="py-5" style="background-image: url('./assets/img/bg/hero.png')">
       <div class="container px-4 px-lg-5 mt-5">
-        <div
-          class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-3 justify-content-center"
-        >
+        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-3 justify-content-center">
           <div class="col mb-5">
             <div class="card h-100 bg-medcream border border-0">
               <!-- Product image-->
-              <img
-                class="card-img-top"
-                src="./assets/img/thumbnail/breads.jpg"
-                alt="..."
-              />
+              <img class="card-img-top" src="./assets/img/thumbnail/breads.jpg" alt="..." />
               <!-- Product details-->
               <div class="card-body p-4">
                 <div class="text-center">
@@ -106,9 +111,7 @@
               <!-- Product actions-->
               <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div class="text-center">
-                  <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-breads.php"
-                    >View Details</a
-                  >
+                  <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-breads.php">View Details</a>
                 </div>
               </div>
             </div>
@@ -116,11 +119,7 @@
           <div class="col mb-5">
             <div class="card h-100 bg-medcream border border-0">
               <!-- Product image-->
-              <img
-                class="card-img-top"
-                src="./assets/img/thumbnail/pd.jpg"
-                alt="..."
-              />
+              <img class="card-img-top" src="./assets/img/thumbnail/pd.jpg" alt="..." />
               <!-- Product details-->
               <div class="card-body p-4">
                 <div class="text-center">
@@ -131,9 +130,7 @@
               <!-- Product actions-->
               <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div class="text-center">
-                <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-breads.php"
-                    >View Details</a
-                  >
+                  <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-pastry.php">View Details</a>
                 </div>
               </div>
             </div>
@@ -141,11 +138,7 @@
           <div class="col mb-5">
             <div class="card h-100 bg-medcream border border-0">
               <!-- Product image-->
-              <img
-                class="card-img-top"
-                src="./assets/img/thumbnail/cakes.jpg"
-                alt="..."
-              />
+              <img class="card-img-top" src="./assets/img/thumbnail/cakes.jpg" alt="..." />
               <!-- Product details-->
               <div class="card-body p-4">
                 <div class="text-center">
@@ -156,9 +149,7 @@
               <!-- Product actions-->
               <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div class="text-center">
-                <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-breads.php"
-                    >View Details</a
-                  >
+                  <a class="btn bg-darkbrown fw-medium text-light mt-auto" href="./menu/menu-cakes.php">View Details</a>
                 </div>
               </div>
             </div>
